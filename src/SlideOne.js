ecbd5de4dc2694f96a079202cb9ef32e7acc8679
ModOst7 <karasique359@yandex.ru>
2022-06-13 17:42:25 +0400

    2 version

--- src/SlideOne.js
+++ src/SlideOne.js
@@ -1,7 +1,23 @@
 import {useSpring, useSpringRef, useChain, animated} from 'react-spring';
 
 export default function SlideOne({one, two, slideOneToggle, slideTwoToggle}){
+    const slideOneRef = useSpringRef();
+    const leftMessageRef = useSpringRef();
+    const arrowNextRef = useSpringRef();
+    const emotionsRef = useSpringRef();
+    const teacherOneRef = useSpringRef();
+    const teacherTwoRef = useSpringRef();
+    const rightMessageRef = useSpringRef();
+
+    const propsLeftMessage = useSpring({ref: leftMessageRef, to: {opacity: 1, left: '10%'}, from: {opacity: 0, left: '-60%'}});
+    const propsTeacherOne = useSpring({ref: teacherOneRef, to: {opacity: 1, left: '14%', marginTop: '17%'}, from: {opacity: 0, left: '-60%', marginTop: '60%'}});
+    const propsRightMessage = useSpring({ref: rightMessageRef, to: {opacity: 1, left: '54%', marginTop: '37.5%'}, from: {opacity: 0, left: '80', marginTop: '60%'}});
+    const propsTeacherTwo = useSpring({ref: teacherTwoRef, to: {opacity: 1, left: '59.2%'}, from: {opacity: 0, left: '80%'}});
+    const propsEmotions = useSpring({ref: emotionsRef, to: {opacity: 1}, from: {opacity: 0}});
+    const propsArrowNext = useSpring({ref: arrowNextRef, to: {opacity: 1}, from: {opacity: 0}});
+
     const propsSlideOne = useSpring({
+        ref: slideOneRef,
         to: {
             opacity: one? 1 : 0, 
             display: one? 'block' : 'none' 
@@ -17,12 +33,14 @@ export default function SlideOne({one, two, slideOneToggle, slideTwoToggle}){
         slideTwoToggle(true);
     }
 
+    useChain([slideOneRef, leftMessageRef, teacherOneRef, rightMessageRef, teacherTwoRef, emotionsRef, arrowNextRef], [0, 0.3, 0.5, 0.9, 1.1, 1.3, 1.8]);
+
     return <animated.div className='slide-1' style={propsSlideOne}>
-    <animated.img className='slide-one-emotions' src='./img/slide1_emotions.svg'></animated.img>
-    <animated.img className='slide-one-arrow-next' src='./img/slide1_arrow.svg' onClick={slideOneClick} ></animated.img>
-    <animated.img className='slide-one-left-message' src='./img/slide1_left-message.svg'></animated.img>
-    <animated.img className='slide-one-teacher-1' src='./img/slide1_teacher1.svg'></animated.img>
-    <animated.img className='slide-one-teacher-2' src='./img/slide1_teacher2.svg'></animated.img>
-    <animated.img className='slide-one-right-message' src='./img/slide1_right-message.svg'></animated.img>
+    <animated.img style={propsEmotions} className='slide-one-emotions' src='./img/slide1_emotions.svg'></animated.img>
+    <animated.img style={propsArrowNext} className='slide-one-arrow-next' src='./img/slide1_arrow.svg' onClick={slideOneClick} ></animated.img>
+    <animated.img style={propsLeftMessage} className='slide-one-left-message' src='./img/slide1_left-message.svg'></animated.img>
+    <animated.img style={propsTeacherOne} className='slide-one-teacher-1' src='./img/slide1_teacher1.svg'></animated.img>
+    <animated.img style={propsTeacherTwo} className='slide-one-teacher-2' src='./img/slide1_teacher2.svg'></animated.img>
+    <animated.img style={propsRightMessage} className='slide-one-right-message' src='./img/slide1_right-message.svg'></animated.img>
   </animated.div>
 }
--- src/SlideTwo.js
+++ src/SlideTwo.js
@@ -1,7 +1,35 @@
 import {useSpring, useSpringRef, useChain, animated} from 'react-spring';
+import {useEffect} from 'react';
 
 export default function SlideTwo({one, two, slideOneToggle, slideTwoToggle, slideThreeToggle, toggleTitle}){
+    const slideTwoRef = useSpringRef();
+    const leftMessageRef = useSpringRef();
+    const rightMessageRef = useSpringRef();
+    const leftKidsRef = useSpringRef();
+    const rightKidRef = useSpringRef();
+    const molniyaRef = useSpringRef();
+    const teacherRef = useSpringRef();
+    const arrowLeftRef = useSpringRef();
+    const arrowRightRef = useSpringRef();
+    const kidContainerRef = useSpringRef();
+
+    const propsLeftMessage = useSpring({ref: leftMessageRef, to: {opacity: 1, left: '3%'}, from: {opacity: 0, left: '-60%'}});
+    const propsLeftKids = useSpring({ref: leftKidsRef, to: {opacity: 1}, from: {opacity: 0}});
+    const propsTeacher = useSpring({ref: teacherRef, to: {opacity: 1}, from: {opacity: 0}});
+    const propsMolniya = useSpring({ref: molniyaRef, to: {opacity: 1}, from: {opacity: 0}, config: {duration: 1000}});
+    const propsRightKid = useSpring({ref: rightKidRef, to: {left: '55%'}, from: {left:'49%'}, config: {duration: 3000}});
+    const propsRightMessage = useSpring({ref: rightMessageRef, to: {opacity: 1, left: '57%'}, from: {opacity: 0, left: '80%'}});
+    const propsArrowLeft = useSpring({ref: arrowLeftRef, to: {opacity: 1}, from: {opacity: 0}});
+    const propsArrowRight = useSpring({ref: arrowRightRef, to: {opacity: 1}, from: {opacity: 0}});
+    const propsKidContainer = useSpring({ref: kidContainerRef, to: {opacity: 1}, from: {opacity: 0}, config: {duration: 2000}});
+
+       
+
+        
+
+
     const propsSlideTwo = useSpring({
+        ref: slideTwoRef,
         to: {
             opacity: two? 1 : 0, 
             display: two? 'block' : 'none' 
@@ -11,6 +39,8 @@ export default function SlideTwo({one, two, slideOneToggle, slideTwoToggle, slid
             display: two? 'none' : 'block'
         }
     });
+
+    useChain(two? [slideTwoRef, leftKidsRef, leftMessageRef, teacherRef, molniyaRef, kidContainerRef, rightKidRef, rightMessageRef, arrowLeftRef, arrowRightRef] : [slideTwoRef], [0, 0.3, 0.6, 1.5, 1.55, 2.0, 2.0, 3.0, 3.5, 3.6]);
 
     const slideTwoClickPrev = () => {
         slideTwoToggle(false);
@@ -24,13 +54,15 @@ export default function SlideTwo({one, two, slideOneToggle, slideTwoToggle, slid
     }
 
     return  <animated.div className='slide-2' style={propsSlideTwo}>
-        <animated.img className='slide-two-left-kids' src='./img/slide2_left-kids.svg'></animated.img>
-        <animated.img className='slide-two-left-message' src='./img/slide2_left-message.svg'></animated.img>
-        <animated.img className='slide-two-right-kid' src='./img/slide2_right-kid997.svg'></animated.img>
-        <animated.img className='slide-two-molniya' src='./img/slide2_molniya.svg'></animated.img>
-        <animated.img className='slide-two-right-message' src='./img/slide2_right-message.svg'></animated.img>
-        <animated.img className='slide-two-teacher' src='./img/slide2_teacher.svg'></animated.img>
-        <animated.img className='slide-two-arrow-left' src='./img/slide2_arrow-left.svg' onClick={slideTwoClickPrev} ></animated.img>
-        <animated.img className='slide-two-arrow-right' src='./img/slide2_arrow-right.svg' onClick={slideTwoClickNext} ></animated.img>
+        <animated.img style={propsLeftKids} className='slide-two-left-kids' src='./img/slide2_left-kids.svg'></animated.img>
+        <animated.img style={propsLeftMessage} className='slide-two-left-message' src='./img/slide2_left-message.svg'></animated.img>
+        <animated.div style={propsKidContainer}>
+            <animated.img style={propsRightKid} className='slide-two-right-kid' src='./img/slide2_right-kid997.svg'></animated.img>
+        </animated.div>
+        <animated.img style={propsMolniya} className='slide-two-molniya' src='./img/slide2_molniya.svg'></animated.img>
+        <animated.img style={propsRightMessage} className='slide-two-right-message' src='./img/slide2_right-message.svg'></animated.img>
+        <animated.img style={propsTeacher} className='slide-two-teacher' src='./img/slide2_teacher.svg'></animated.img>
+        <animated.img style={propsArrowLeft} className='slide-two-arrow-left' src='./img/slide2_arrow-left.svg' onClick={slideTwoClickPrev} ></animated.img>
+        <animated.img style={propsArrowRight} className='slide-two-arrow-right' src='./img/slide2_arrow-right.svg' onClick={slideTwoClickNext} ></animated.img>
       </animated.div>
 }
